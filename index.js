6d8aee269fc305816ceb7aaccebc051dba5fd428
Brian Brunner <brian.brunner@gmail.com>
2016-05-02 08:50:06 -0700

    Moved to context.session

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var _ = require('lodash');
-var contexts = {};
+var sessions = {};
 
 module.exports = function (context, event, next) {
   // create unique id for the current platform/session combo
-  var id = context._platform.name + "|" + context.sessionId;
-  var savedContext = (id in contexts) ? contexts[id] : {};
-  context.merge(savedContext);
+  var id = context.platform.name + "|" + context.sessionId;
+  var savedSession = (id in contexts) ? session[id] : {};
+  context.mergeSession(savedSession);
   next(null, function(context, event, next) {
-    contexts[id] = context.dump();
+    sessions[id] = context.dumpSession();
     next();
   });
 };
