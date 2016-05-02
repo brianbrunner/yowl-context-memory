bc5ca6858613aee546f83793a765f48db0721a1c
Brian Brunner <brian.brunner@gmail.com>
2016-05-02 08:59:48 -0700

    Fixed error with nonexistant variable

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var sessions = {};
 module.exports = function (context, event, next) {
   // create unique id for the current platform/session combo
   var id = context.platform.name + "|" + context.sessionId;
-  var savedSession = (id in contexts) ? session[id] : {};
+  var savedSession = (id in sessions) ? session[id] : {};
   context.mergeSession(savedSession);
   next(null, function(context, event, next) {
     sessions[id] = context.dumpSession();
